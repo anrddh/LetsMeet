ea93afa9ad47f27cb99ff04de34467c814cd741f
Aniruddh Agarwal <aaniruddh99@gmail.com>
2016-06-02 20:46:29 +0800

    Add file loader for image processing

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: 'url?limit=10000!img?progressive=true',
+        loader: 'file-loader',
       },
     ],
   },
